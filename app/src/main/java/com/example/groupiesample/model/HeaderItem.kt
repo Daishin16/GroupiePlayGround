e3902dd45f9fbdf1c0cbbc1b4f502df4a1c1243c
Daishin16 <sakuya1202@icloud.com>
2020-05-09 19:28:53 +0900

    Problem / No sugguestion of BindableItem

---
+++ app/src/main/java/com/example/groupiesample/model/HeaderItem.kt
@@ -0,0 +1,5 @@
+package com.example.groupiesample.model
+
+class HeaderItem {
+
+}
---
+++ app/src/main/java/com/example/groupiesample/model/TextItem.kt
@@ -0,0 +1,8 @@
+package com.example.groupiesample.model
+
+import com.example.groupiesample.databinding.ItemTextBinding
+import com.xwray.groupie.
+
+data class TextItem(val text: String) : BindableItem {
+
+}
